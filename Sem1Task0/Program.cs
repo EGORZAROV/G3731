981eb2bc7df4497e6f6b5d7015dd1825eb56d2a8
Egor <e.kaplunov@gmail.com>
2023-01-20 17:40:23 +0300

    add solution for Task 0

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача # 0
+
+string? inputNum =Console.ReadLine();
+
+if(inputNum!=null)
+{
+// Парсим введенное число 
+int number = int.Parse(inputNum);
+// находим квадрат числа
+// int result = number*number;
+// вводим данные в консоль
+
+int result = (int)Math.Pow(number,2);
+
+Console.WriteLine(result);
+}
