7d2b554e1dae0e6140d70b3ffdf82a7a10e446ae
Egor <e.kaplunov@gmail.com>
2023-01-31 16:19:06 +0300

    add solution for task 23,33

--- Sem1Task4/Program.cs
+++ Sem1Task4/Program.cs
@@ -1,21 +1,41 @@
-﻿// Написать программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+﻿// // Написать программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
 
-string? inputNum1 = Console.ReadLine();
-string? inputNum2 = Console.ReadLine();
-string? inputNum3 = Console.ReadLine();
+// string? inputNum1 = Console.ReadLine();
+// string? inputNum2 = Console.ReadLine();
+// string? inputNum3 = Console.ReadLine();
 
-if(inputNum1!=null && inputNum2!=null && inputNum3!=null)
+// if(inputNum1!=null && inputNum2!=null && inputNum3!=null)
 
+// {
+// int namber1 = int.Parse(inputNum1);
+// int namber2 = int.Parse(inputNum2);
+// int namber3 = int.Parse(inputNum3);
+
+// if (namber1 > namber2)
+// {
+
+// if (namber1 > namber3)
+
+// }
+// Console.WriteLine("");
+// }
+
+int[] nums = new int[3]; // Обьявляем массив данных
+
+for (int i=0; i<3; i++)
 {
-int namber1 = int.Parse(inputNum1);
-int namber2 = int.Parse(inputNum2);
-int namber3 = int.Parse(inputNum3);
+    Console.WriteLine("Введите "+(i+1)+"-е число:"); // просим пользователя ввести (i+1)-е число
+    nums[i]=int.Parse(Console.ReadLine()??"0"); // считываем данные в массив
+}
 
-if (namber1 > namber2)
+int max=nums[0]; // создаем переменную для макс. значения и приравниваем её к 0му элементу массива 
+
+for (int i=1; i<3; i++) // запускаем цикл по массиву, начиная с 1-го (не 0) элемента, т.к. 0й элемент уже записан в max
 {
-
-if (namber1 > namber3)
-
+    if (nums[i]>max) // если текущий элемент больше max, то запоминаем новый максимум
+    {
+        max=nums[i];
+    }
 }
-Console.WriteLine("");
-}
+
+Console.WriteLine("Максимальное значение = "+max); // выводим результат
--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -1,13 +1,13 @@
-﻿// Задача 6
+﻿// // Задача 6
 
-string? inputNum1 = Console.ReadLine()??"0";
-int namber1 = int.Parse(inputNum1);
+// Ввод числа пользователем:
+Console.WriteLine("Введите число:");
+int num1 = int.Parse(Console.ReadLine()??"0");
 
-if (number1%2==0)
+if (num1%2==0) // если остаток от целочисленного деления числа на 2 = 0, то
 {
-Console.WriteLine("четное");
+    Console.WriteLine("Число чётное"); //выводим, что число чётное
 }
-else
-{
-Console.WriteLine("не четное");
+else{
+    Console.WriteLine("Число не чётное"); // иначе число не чётное
 }
